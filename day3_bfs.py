ebcc4edfd48c9f98941f1414009c249ecaf3ba7c
MarinaSergeeva <sergeeva.marin@gmail.com>
2017-10-14 00:30:07 -0400

    added day3: bfs

---
+++ day3_bfs.py
@@ -0,0 +1,26 @@
+import collections
+
+def bfs(graph, source):
+    visited = set()
+    discovered = set([source])
+    queue = collections.deque()
+    queue.append(source)
+    while queue:
+        v = queue.popleft()
+        visited.add(v)
+        # print(v)
+        for el in graph[v]:
+            if el not in discovered:
+                queue.append(el)
+                discovered.add(el)
+                
+def test_bfs():
+    graph = {0: [1, 2],
+            1: [4],
+            2: [3],
+            3: [2],
+            4:[]}
+    bfs(graph, 0)
+
+if __name__ == "__main__":
+    test_bfs()
